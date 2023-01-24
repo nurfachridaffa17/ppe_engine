152a0637b630775aa9496e59735cb583d8d2d8e0
Nurfachri <nurfachridaffa17@gmail.com>
2023-01-24 09:27:15 +0000

    refactory code

--- engine_ppe/views.py
+++ engine_ppe/views.py
@@ -24,7 +24,13 @@ def get_token():
     with open("engine_ppe/config.json", "w") as f:
         json.dump(config, f)
     
-    return make_response(jsonify({'status': 200, 'message': 'Token generated successfully', 'api_token': api_token}), 200)
+    return make_response(jsonify(
+        {
+            'status': 200, 
+            'message': 'Token generated successfully', 
+            'api_token': api_token
+        }
+        ), 200)
 
 @app.route('/api/v1/upload/<path:filename>', methods=['GET'])
 def upload_image(filename):
@@ -33,35 +39,63 @@ def upload_image(filename):
         config = json.load(f)
     
     if authorization != config["api_token"]:
-        return make_response(jsonify({'status': 401, 'message': 'Unauthorized'}), 401)
+        return make_response(jsonify(
+            {
+                'status': 401, 
+                'message': 'Unauthorized'
+            }
+            ), 401)
     
     try:
         return send_from_directory('static/image_out', filename, as_attachment=True)
     except FileNotFoundError:
-        return make_response(jsonify({'status': 404, 'message': 'File not found'}), 404)
+        return make_response(jsonify(
+            {
+                'status': 404, 
+                'message': 'File not found'
+            }
+            ), 404)
 
 
-@app.route('/api/v1/process/image', methods=['POST', 'GET'])
+@app.route('/api/v1/process/image', methods=['POST'])
 def get_image():
-
     authorization = request.headers.get('Authorization')
     with open("engine_ppe/config.json", "r") as f:
         config = json.load(f)
     
     if authorization != config["api_token"]:
-        return make_response(jsonify({'status': 401, 'message': 'Unauthorized'}), 401)
+        return make_response(jsonify(
+            {
+                'status': 401, 
+                'message': 'Unauthorized'
+            }
+            ), 401)
     elif authorization is None:
-        return make_response(jsonify({'status': 401, 'message': 'Unauthorized'}), 401)
+        return make_response(jsonify(
+            {
+                'status': 401, 
+                'message': 'Unauthorized'
+            }
+            ), 401)
 
 
     if 'file_image' not in request.files:
-        return jsonify({'status': 400, 'message': 'No file part'})
+        return make_response(jsonify(
+            {
+                'status': 400, 
+                'message': 'No file part'
+            }
+            ), 400)
 
     file = request.files['file_image']
-    # file = request.data
 
     if file.filename == '':
-        return make_response(jsonify({'status': 400, 'message': 'No selected file'}), 400)
+        return make_response(jsonify(
+            {
+                'status': 400, 
+                'message': 'No selected file'
+            }
+            ), 400)
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
@@ -78,8 +112,10 @@ def get_image():
     detector.setJsonPath(json_path)
     detector.loadModel()
     detector.useCPU()
-    detections = detector.detectObjectsFromImage(input_image="engine_ppe/static/image_in/{}".format(nama_img),
-                                    output_image_path="engine_ppe/static/image_out/{}".format(nama_img),)
+    detections = detector.detectObjectsFromImage(
+        input_image="engine_ppe/static/image_in/{}".format(nama_img), 
+        output_image_path="engine_ppe/static/image_out/{}".format(nama_img),
+        )
     
     safety_list = []
 
@@ -97,13 +133,45 @@ def get_image():
     not_safety_shoes = "safety_shoes" not in safety_list
     
     if safety_helmet and safety_shoes:
-        return make_response(jsonify({'status': 200, 'message': 'Image processed successfully', "safety_helmet": True, "safety_shoes": True, "link" : link}),200)
+        return make_response(jsonify(
+            {
+                'status': 200, 
+                'message': 'Image processed successfully', 
+                "safety_helmet": True, 
+                "safety_shoes": True, 
+                "link" : link
+            }
+            ),200)
     elif not_safety_helmet and safety_shoes:
-        return make_response(jsonify({'status': 200, 'message': 'Image processed successfully', "safety_helmet": False, "safety_shoes": True, "link" : link}),200)
+        return make_response(jsonify(
+            {
+                'status': 200, 
+                'message': 'Image processed successfully', 
+                "safety_helmet": False, 
+                "safety_shoes": True, 
+                "link" : link
+            }
+            ),200)
     elif safety_helmet and not_safety_shoes:
-        return make_response(jsonify({'status': 200, 'message': 'Image processed successfully', "safety_helmet": True, "safety_shoes": False, "link" : link}),200)
+        return make_response(jsonify(
+            {
+                'status': 200, 
+                'message': 'Image processed successfully', 
+                "safety_helmet": True, 
+                "safety_shoes": False, 
+                "link" : link
+            }
+            ),200)
     else:
-        return make_response(jsonify({'status': 200, 'message': 'Image processed successfully', "safety_helmet": False, "safety_shoes": False, "link" : link}),200)
+        return make_response(jsonify(
+            {
+                'status': 200, 
+                'message': 'Image processed successfully', 
+                "safety_helmet": False, 
+                "safety_shoes": False, 
+                "link" : link
+            }
+            ),200)
 
 
 @app.errorhandler(404)
@@ -112,11 +180,21 @@ def not_found(error):
 
 @app.errorhandler(500)
 def internal_error(error):
-    return make_response(jsonify({'status': 500, 'message': 'Internal Server Error'}), 500)
+    return make_response(jsonify(
+        {
+            'status': 500, 
+            'message': 'Internal Server Error'
+        }
+        ), 500)
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify({'status': 400, 'message': 'Bad Request'}), 400)
+    return make_response(jsonify(
+        {
+            'status': 400, 
+            'message': 'Bad Request'
+        }
+        ), 400)
 
     
 
